c4855e60ebc4c1ff7b8a665ad0c1483763c74ac6
Fary02 <267945234+Fary02@users.noreply.github.com>
2026-05-13 09:52:25 -0400

    Add files via upload

---
+++ function-saludo.py
@@ -0,0 +1,15 @@
+# Se define la function y se asigna parametro;  
+
+def saludo (a):
+    
+    # Se retornan los parametros;
+    
+    return a; 
+  
+# Solicitud de ingreso de datos;
+
+saludar = str(input("Saludame: "));  
+
+# Se imprime el resultado;
+
+print("Resultado:", saludo(saludar)); 
